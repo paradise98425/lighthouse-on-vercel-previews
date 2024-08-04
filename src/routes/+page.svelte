cc0c063c03ba52b0bfb5e9e7a818a74a866ce569
Roshan Pratap Katel <roshanpratapkatel@roshans-macbook-air.home>
2024-08-04 13:53:05 +0200

    add comment to trigger new builds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
+<!-- Just to trigger new build in svelte Kit -->
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>This is for all the github actions and vercel related R&D</p>
